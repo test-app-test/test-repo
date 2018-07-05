942de3d3b1cfb9d1e87a2a83966c2d947dbfbd4e
Marko Nazarii <physicist1m@github.com>
2018-07-05 13:13:36 +0300

    adding comment

--- src/components/headerV2/headerV2.js
+++ src/components/headerV2/headerV2.js
@@ -4,6 +4,6 @@ export default () => {
     return (
         <div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio laudantium pariatur fuga delectus laborum cumque a, minima quia ipsum dolor officia iusto, vero fugit at suscipit blanditiis similique. Minima, ut.</p>
-        </div>
+        </div>//hello
     );
 }
